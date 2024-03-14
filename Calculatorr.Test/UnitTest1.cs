156512640b97846ea40fde25fc15eb11b879a458
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-03-14 08:47:46 +0500

    Updated

--- Calculatorr.Test/UnitTest1.cs
+++ Calculatorr.Test/UnitTest1.cs
@@ -5,8 +5,8 @@ namespace Calculatorr.Test
     public class UnitTest1
     {
         [Theory]
-        [InlineData(2, 2, 4)]
-        [InlineData(2, 3, 5)]
+        [InlineData(4, 5, 9)]
+        [InlineData(1, 4, 5)]
         public void TestAdd(double a, double b, double expactedResult)
         {
             var result = Calculator.Add(a, b);
@@ -14,24 +14,24 @@ namespace Calculatorr.Test
         }
 
         [Theory]
-        [InlineData(2, 2, 0)]
-        [InlineData(2, 3, -1)]
+        [InlineData(3, 2, 1)]
+        [InlineData(4, 4, 0)]
         public void TestSubtract(double a, double b, double expactedResult)
         {
             var result = Calculator.Subtract(a, b);
             Assert.Equal(expactedResult, result);
         }
         [Theory]
-        [InlineData(2, 2, 1)]
-        [InlineData(34, 2, 17)]
+        [InlineData(4, 2, 2)]
+        [InlineData(100, 2, 50)]
         public void TestDevide(double a, double b, double expactedResult)
         {
             var result = Calculator.Devide(a, b);
             Assert.Equal(expactedResult, result);
         }
         [Theory]
-        [InlineData(2, 2, 4)]
-        [InlineData(2, 3, 6)]
+        [InlineData(2, 5, 10)]
+        [InlineData(4, 3, 12)]
         public void TestMultiply(double a, double b, double expactedResult)
         {
             var result = Calculator.Multiply(a, b);
