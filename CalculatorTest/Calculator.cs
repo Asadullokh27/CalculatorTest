f7174a55645abb22577381b8169ec6e9ac7b7324
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-03-14 07:09:30 +0500

    Add project files.

---
+++ CalculatorTest/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTest
+{
+    public class Calculator
+    {
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Devide(double a, double b)
+        {
+            return a / b;
+        }
+
+    }
+}
