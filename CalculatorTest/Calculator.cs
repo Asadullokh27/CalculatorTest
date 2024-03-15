736de283d45a96c973a9e6beb1ed0bfdf1f12647
Asadullokh27 <asadullohtojiyev696@gmail.com>
2024-03-15 09:33:41 +0500

    Optimal

--- CalculatorTest/Calculator.cs
+++ CalculatorTest/Calculator.cs
@@ -1,30 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CalculatorTest
 {
     public class Calculator
     {
-
         public static double Add(double a, double b)
         {
             return a + b;
         }
+
         public static double Subtract(double a, double b)
         {
             return a - b;
         }
+
         public static double Multiply(double a, double b)
         {
             return a * b;
         }
-        public static double Devide(double a, double b)
+
+        public static double Divide(double a, double b)
         {
+            if (b == 0)
+            {
+                throw new ArgumentException("Cannot divide by zero.");
+            }
             return a / b;
         }
 
+        public static double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public static double SquareRoot(double a)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Cannot calculate square root of a negative number.");
+            }
+            return Math.Sqrt(a);
+        }
+
+        public static double Modulus(double a, double b)
+        {
+            return a % b;
+        }
     }
 }
--- Calculatorr.Test/UnitTest1.cs
+++ Calculatorr.Test/UnitTest1.cs
@@ -1,4 +1,5 @@
 using CalculatorTest;
+using Xunit;
 
 namespace Calculatorr.Test
 {
@@ -7,35 +8,64 @@ namespace Calculatorr.Test
         [Theory]
         [InlineData(4, 5, 9)]
         [InlineData(1, 4, 5)]
-        public void TestAdd(double a, double b, double expactedResult)
+        public void TestAdd(double a, double b, double expectedResult)
         {
             var result = Calculator.Add(a, b);
-            Assert.Equal(expactedResult, result);
+            Assert.Equal(expectedResult, result);
         }
 
         [Theory]
         [InlineData(3, 2, 1)]
         [InlineData(4, 4, 0)]
-        public void TestSubtract(double a, double b, double expactedResult)
+        public void TestSubtract(double a, double b, double expectedResult)
         {
             var result = Calculator.Subtract(a, b);
-            Assert.Equal(expactedResult, result);
+            Assert.Equal(expectedResult, result);
         }
+
         [Theory]
         [InlineData(4, 2, 2)]
         [InlineData(100, 2, 50)]
-        public void TestDevide(double a, double b, double expactedResult)
+        public void TestDivide(double a, double b, double expectedResult)
         {
-            var result = Calculator.Devide(a, b);
-            Assert.Equal(expactedResult, result);
+            var result = Calculator.Divide(a, b);
+            Assert.Equal(expectedResult, result);
         }
+
         [Theory]
         [InlineData(2, 5, 10)]
         [InlineData(4, 3, 12)]
-        public void TestMultiply(double a, double b, double expactedResult)
+        public void TestMultiply(double a, double b, double expectedResult)
         {
             var result = Calculator.Multiply(a, b);
-            Assert.Equal(expactedResult, result);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(2, 3, 8)]
+        [InlineData(4, 2, 16)]
+        public void TestPower(double a, double b, double expectedResult)
+        {
+            var result = Calculator.Power(a, b);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(16, 4)]
+        [InlineData(25, 5)]
+        public void TestSquareRoot(double a, double expectedResult)
+        {
+            var result = Calculator.SquareRoot(a);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(10, 3, 1)]
+        [InlineData(20, 7, 6)]
+        public void TestModulus(double a, double b, double expectedResult)
+        {
+            var result = Calculator.Modulus(a, b);
+            Assert.Equal(expectedResult, result);
         }
     }
 }
